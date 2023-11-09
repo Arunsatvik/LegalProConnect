5379a8b02c15c7a5a9a8d64a80f787b9e797db33
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2023-11-08 16:41:21 -0800

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5001/api/v1";
+export const BASE_URL = "http://ec2-18-234-45-4.compute-1.amazonaws.com:5001/api/v1";
 export const token = localStorage.getItem("token");
