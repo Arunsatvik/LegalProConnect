38bcb3f419e443a55354c55584532165e402cd75
Arunsatvik <64481770+Arunsatvik@users.noreply.github.com>
2023-11-16 18:13:12 -0800

    Update ChatUI.jsx

--- frontend/src/components/ChatUI/ChatUI.jsx
+++ frontend/src/components/ChatUI/ChatUI.jsx
@@ -18,7 +18,7 @@ const ChatUI = () => {
         setChatHistory((prevHistory) => [...prevHistory, newMessage]);
         setUserInput('');
 
-        const apiUrl = `http://ec2-3-82-21-179.compute-1.amazonaws.com:8080/bot/chat?prompt=${encodeURIComponent(userInput)}`;
+        const apiUrl = `https://ec2-3-82-21-179.compute-1.amazonaws.com:8080/bot/chat?prompt=${encodeURIComponent(userInput)}`;
 
         try {
             const res = await fetch(apiUrl, {
@@ -27,19 +27,28 @@ const ChatUI = () => {
                     'Accept': 'text/plain'
                 }
             });
-
+        
             if (res.ok) {
                 const data = await res.text();
-                const botResponse = { text: data, type: 'bot' };
-
+                console.log(data)
+                // Replace newline characters with <br> tags
+                const formattedData = data.replace(/\n/g, '<br>');
+                // console.log(formattedData)
+        
+                // Create a new message with formatted text
+                const botResponse = {
+                    text: formattedData,
+                    type: 'bot',
+                    id: 'bot'
+                };
                 setChatHistory((prevHistory) => [...prevHistory, botResponse]);
             } else {
-                const errorMessage = { text: "No results found.", type: 'bot' };
+                const errorMessage = { text: "No results found.", type: 'bot', id: 'error' };
                 setChatHistory((prevHistory) => [...prevHistory, errorMessage]);
             }
         } catch (error) {
             console.error("There was an error sending the data:", error);
-            const errorMessage = { text: "An error occurred while processing your request.", type: 'bot' };
+            const errorMessage = { text: "An error occurred while processing your request.", type: 'bot', id: 'error' };
             setChatHistory((prevHistory) => [...prevHistory, errorMessage]);
         } finally {
             setLoading(false);
@@ -47,37 +56,39 @@ const ChatUI = () => {
     };
 
     return (
-    <div className="chat-container">
-        <header className="chat-header">
-            LegalProConnect AI
-        </header>
+        <div className="chat-container">
+            <header className="chat-header">
+                LegalProConnect AI
+            </header>
 
-        <div className="chat-body">
-            <div className="chat-history">
-                {chatHistory.map((message, index) => (
-                    <div key={index} className={`message ${message.type}`}>
-                        {message.text}
-                    </div>
-                ))}
-                
-                {loading && (
-                    <div className="loading-spinner">
-                        <HashLoader color="#3364A8" size={40} />
-                    </div>
-                )}
-            </div>
+            <div className="chat-body">
+                <div className="chat-history">
+                    {chatHistory.map((message) => (
+                        <div
+                            key={message.id}
+                            className={`message ${message.type}`}
+                            dangerouslySetInnerHTML={{ __html: message.text }}
+                        />
+                    ))}
 
-            <div className="input-area">
-                <textarea
-                    placeholder="Type your legal-related question..."
-                    value={userInput}
-                    onChange={handleInputChange}
-                ></textarea>
-                <button className="send-button" onClick={handleSendClick}>Send</button>
+                    {loading && (
+                        <div className="loading-spinner">
+                            <HashLoader color="#3364A8" size={40} />
+                        </div>
+                    )}
+                </div>
+
+                <div className="input-area">
+                    <textarea
+                        placeholder="Type your legal-related question..."
+                        value={userInput}
+                        onChange={handleInputChange}
+                    ></textarea>
+                    <button className="send-button" onClick={handleSendClick}>Send</button>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default ChatUI;
